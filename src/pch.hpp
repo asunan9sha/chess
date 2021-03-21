1d98bd6261b26ff6eae2fed25600f86bd0943e1a
Asunan9sha <asunan9sha@gmail.com>
2021-03-21 18:07:09 +0200

    added pch

---
+++ src/pch.hpp
@@ -0,0 +1,17 @@
+#ifndef CHESS_PCH_HPP
+#define CHESS_PCH_HPP
+
+
+#include <functional>
+#include <memory>
+#include <unordered_map>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "utils/types.hpp"
+
+
+#endif
